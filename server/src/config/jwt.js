009c84c9c84249196dd7bd75e801c6634a7958c3
szbr1 <szb8480@gmail.com>
2025-07-23 12:58:51 +0500

    chore: refine the code and middleware

--- server/src/config/jwt.js
+++ server/src/config/jwt.js
@@ -5,9 +5,16 @@ const AssignToken = (res,userId,next)=>{
         if(!userId){
             return res.status(400).json("User Not Found")
         }
-         const token = jwt.sign({userId},process.env.JWT_ASSIGN, {
-            expiresIn: "30d"
-        })
+         const token = jwt.sign(
+            {userId},process.env.JWT_ASSIGN, 
+            {expiresIn: "30d"})
+
+            res.cookie("token", token, {
+                maxAge: "30d",
+                sameSite: "strict",
+                secure: true,
+                httpOnly: true
+            })
     } catch (error) {
         console.error(error, "AssignTokenIssue")
     }
--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -1,82 +1,102 @@
 import User from "../models/user.model.js";
 import validator from "validator";
 import bcrypt from "bcrypt";
-import AssignToken from "../config/jwt.js";
+import AssignTokenToUser from "../config/userJwt.js";
+import AssignTokenToAdmin from "../config/adminJwt.js";
 import passport from "passport";
+
+export const admin = async (req, res) => {
+  // You can implement admin-only logic here later
+};
 
 export const signup = async (req, res) => {
   try {
     const { username, email, password } = req.body;
 
-    let emailCheck = await User.findOne({ email });
+    const emailCheck = await User.findOne({ email });
 
-     if(emailCheck && (emailCheck.githubId || emailCheck.googleId) && !emailCheck.password){
+    // Case: OAuth user wants to set password now
+    if (emailCheck && (emailCheck.githubId || emailCheck.googleId) && !emailCheck.password) {
       if (!validator.isStrongPassword(password, { minLength: 8 })) {
         return res.status(400).json("Please choose strong password");
       }
+
       const salt = await bcrypt.genSalt(10);
       const hashedPassword = await bcrypt.hash(password, salt);
 
-    
-  
-      emailCheck.password = hashedPassword
-      await emailCheck.save()
-      const token = AssignToken(res, emailCheck._id)
-      return res.status(200).json({success: true, token})
+      emailCheck.password = hashedPassword;
+      await emailCheck.save();
 
-      
-     }else{
-      if (!validator.isEmail(email)) {
-        return res.status(400).json("Please enter valid email");
-      }
-      if (!validator.isStrongPassword(password, { minLength: 8 })) {
-        return res.status(400).json("Please choose strong password");
-      }
-  
-      const salt = await bcrypt.genSalt(10);
-      const hashedPassword = await bcrypt.hash(password, salt);
-  
-      const saveUsertoDB = new User({
-        username,
-        email,
-        password: hashedPassword,
-      });
-  
-      await saveUsertoDB.save();
-      const token = AssignToken(res, saveUsertoDB._id);
-      return res.status(200).json({ success: true, saveUsertoDB, token });
-     }
+      const token = emailCheck.admin
+        ? AssignTokenToAdmin(res, emailCheck._id)
+        : AssignTokenToUser(res, emailCheck._id);
+
+      return res.status(200).json({ success: true, user: emailCheck, token });
+    }
+
+    // New user registration
+    if (!validator.isEmail(email)) {
+      return res.status(400).json("Please enter valid email");
+    }
+
+    if (!validator.isStrongPassword(password, { minLength: 8 })) {
+      return res.status(400).json("Please choose strong password");
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    const token = newUser.admin
+      ? AssignTokenToAdmin(res, newUser._id)
+      : AssignTokenToUser(res, newUser._id);
+
+    return res.status(200).json({ success: true, user: newUser, token });
   } catch (error) {
     console.error("signup error", error);
+    return res.status(500).json("Server error");
   }
 };
 
 export const signin = async (req, res) => {
   try {
     const { email, password } = req.body;
-    const IdentiyCheck = await User.findOne({ email });
 
-    if (!IdentiyCheck) {
-      return res.status("401").json("Invalid Credentials. Try to signup");
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json("Invalid credentials. Try signing up.");
     }
+
     if (!validator.isEmail(email)) {
       return res.status(400).json("Please enter valid email");
     }
+
     if (!validator.isStrongPassword(password, { minLength: 8 })) {
       return res.status(400).json("Please choose strong password");
     }
 
-    const PassChecking = await bcrypt.compare(password, IdentiyCheck.password);
+    const isMatch = await bcrypt.compare(password, user.password);
 
-    if (!PassChecking) {
-      return res.status(401).json("Invalid Credentials. Try to signup");
+    if (!isMatch) {
+      return res.status(401).json("Invalid credentials. Try signing up.");
     }
 
-    const token = AssignToken(res, IdentiyCheck._id);
-    return res.status(200).json({ success: true, token });
+    const token = user.admin
+      ? AssignTokenToAdmin(res, user._id)
+      : AssignTokenToUser(res, user._id);
+
+    return res.status(200).json({ success: true, user, token });
   } catch (error) {
-    console.error(error);
-    res.status(500).json("Server Error");
+    console.error("signin error", error);
+    return res.status(500).json("Server error");
   }
 };
 
@@ -84,41 +104,36 @@ export const googleAuth = passport.authenticate("google", {
   scope: ["profile", "email"],
 });
 
-export const googleCallback = (req,res,next)=>{
-     passport.authenticate("google",{session:false}, (err, user)=>{
-        if(!user || err){
-            console.log(err)
-            return res.status(500).json({success:false, message:"try again later"})
-        }
+export const googleCallback = (req, res, next) => {
+  passport.authenticate("google", { session: false }, (err, user) => {
+    if (!user || err) {
+      console.error("Google auth error:", err);
+      return res.status(500).json({ success: false, message: "Try again later" });
+    }
 
-        const token = AssignToken(res, user.id)
+    user.admin
+      ? AssignTokenToAdmin(res, user._id)
+      : AssignTokenToUser(res, user._id);
 
-        res.cookie("token", token, {
-            httpOnly: true,
-            sameSite: "Lax",
-            secure: false,
-        } )
+    res.redirect("http://localhost:3000");
+  })(req, res, next);
+};
 
-        res.redirect("http://localhost:3000")
-     })(req,res,next)
-}
+export const githubAuth = passport.authenticate("github", {
+  scope: ["profile", "email"],
+});
 
-export const githubAuth = passport.authenticate("github", {scope: ["profile", "email"] })
+export const githubCallback = (req, res, next) => {
+  passport.authenticate("github", { session: false }, (err, user) => {
+    if (!user || err) {
+      console.error("GitHub auth error:", err);
+      return res.status(500).json({ success: false, message: "Try again later" });
+    }
 
-export const githubCallback = (req,res,next)=>{
-    passport.authenticate("github",{session:false}, (err,user)=>{
-           if(!user || err ){
-            console.error(err, "github auth error")
-           }
+    user.admin
+      ? AssignTokenToAdmin(res, user._id)
+      : AssignTokenToUser(res, user._id);
 
-           const token = AssignToken(res, user.id)
-
-        res.cookie("token", token, {
-            httpOnly: true,
-            sameSite: "Lax",
-            secure: false
-        })
-
-        res.redirect("http://localhost:3000"); 
-    })(req,res,next)
-}
+    res.redirect("http://localhost:3000");
+  })(req, res, next);
+};
